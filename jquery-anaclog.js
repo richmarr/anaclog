a87b1396f40b6e58c7952f53ea763b7cff75d9f9
Richard Marr <richard.marr@gmail.com>
2011-07-10 10:38:24 +0100

    Added new methods from recipe show page

--- jquery-anaclog.js
+++ jquery-anaclog.js
@@ -1,15 +1,20 @@
 (function($){
 	$.anaclog = {
 		version : "0.0.1",
-		toRad : function(value,unit) {
-			if ( unit == 'Minutes' || unit == 'Seconds' ) return $.anaclog.fractionToRad(value/60);
-			return $.anaclog.fractionToRad(value/12);
+		toRad : function(value,unit,orient) {
+			var converter = orient ? $.anaclog.fractionToOrientedRad : $.anaclog.fractionToRad;
+			if ( unit == 'Minutes' || unit == 'Seconds' ) return converter(value/60);
+			return converter(value/12);
 		},
-		fractionToRad : function(fraction) { return Math.PI * (2.0 * fraction - 0.5); },
-		radial: function( ctx, r, fraction, style ) {
+		fractionToOrientedRad : function(fraction) { return Math.PI * (2.0 * fraction - 0.5); },
+		fractionToRad : function(fraction) { return Math.PI * 2 * fraction; },
+		radialFraction: function( ctx, r, fraction, style ) {
+			$.anaclog.radial( ctx, r, $.anaclog.fractionToRad(fraction), style );
+		},
+		radial: function( ctx, r, rad, style ) {
 			ctx.save();
 			ctx.translate( r, r );
-			ctx.rotate( $.anaclog.fractionToRad(fraction) );
+			ctx.rotate(rad);
 			ctx.strokeStyle = style.color;
 			ctx.lineWidth = style.width || 1;
 			ctx.beginPath();
@@ -19,8 +24,9 @@
 			ctx.restore();
 		},
 		segment: function( ctx, r, segment ) {
-			var startRadians = $.anaclog.toRad(segment.start,segment.unit);
-			var endRadians = $.anaclog.toRad(segment.end,segment.unit);
+			var endRadians, startRadians = $.anaclog.toRad(segment.start,segment.unit,true);
+			if ( segment.end ) endRadians = $.anaclog.toRad(segment.end,segment.unit,true);
+			else if ( segment.time ) endRadians = startRadians + $.anaclog.toRad(segment.time,segment.unit,false);
 			ctx.save();
 			ctx.lineWidth = 1;
 			ctx.strokeStyle = segment.color;
@@ -38,6 +44,152 @@
 			if ( current == segment.start ) $self.trigger("timerStart");
 			else if ( current == segment.end ) $self.trigger("timerEnd");
 		}
+	};
+	$.fn.step = function(){
+		
+		return this.each(function(){
+			
+			var $self = $(this);
+			var self = this,
+				ctx = self.getContext("2d"), 
+				r = parseInt($self.attr('radius'));
+			
+			var now = parseInt($self.attr('now'));
+			var radius = parseInt($self.attr('radius'));
+			var process = {
+				start:parseInt($self.attr('process-start')),
+				end:parseInt($self.attr('process-end'))
+			};
+			var step = {
+				start:parseInt($self.attr('step-start')),
+				end:parseInt($self.attr('step-end'))
+			};
+			var datediff = function(ms,units){
+				if ( units == 'Seconds' ) return Math.round(ms/1000);
+				if ( units == 'Minutes' ) return Math.round(ms/60000);
+				if ( units == 'Hours' ) return Math.round(ms/3600000);
+			};
+			
+			self.setAttribute("width",r*2);
+			self.setAttribute("height",r*2);
+			self.style.width = r*2 + "px";
+			self.style.height = r*2 + "px";
+			
+			//$self.bind();
+			var diff = process.end - process.start
+			var units = ( diff > 3600000 ) ? 'Hours' : ( diff > 60000 ) ? 'Minutes' : 'Seconds';
+			
+			// process
+			$.anaclog.segment( ctx, r, {
+				color:'#d8df93', 
+				radius:1, 
+				start:(new Date(now+process.start))['get'+units](),
+				time:datediff(process.end - process.start, units),
+				unit:units
+			});
+			
+			// step
+			$.anaclog.segment( ctx, r, {
+				color:'#b0c878', 
+				radius:1, 
+				start:(new Date(now+step.start))['get'+units](),
+				time:datediff(step.end - step.start, units),
+				unit:units
+			});
+			
+			// radius
+			//$.anaclog.radial( ctx, r, hand.value(now), hand );
+			
+		});
+	};
+	$.fn.arcs = function( o ){
+		o = $.extend({
+			radius:50,
+			segments : {}
+		}, o );
+		return this.each(function(){ 
+			
+			var self = this, 
+				$self = $(this), 
+				ctx = self.getContext("2d"), 
+				r = o.radius;
+				
+			self.setAttribute("width",r*2);
+			self.setAttribute("height",r*2);
+			self.style.width = r*2 + "px";
+			self.style.height = r*2 + "px";
+			
+			$self.bind();
+			
+			
+			var now = new Date();
+			
+			//clear last frame
+			//ctx.clearRect( 0, 0, 2*r, 2*r );
+			
+			// draw timers
+			for ( var name in o.segments ) {
+				var segment = o.segments[name];
+				$.anaclog.checkTimer($self, now, segment);
+				$.anaclog.segment( ctx, r, segment );
+			}
+			// draw ticks
+			//for ( var i = 0; i < 60; i++ ) {
+			//	var mark = (i%5) ? o.marks.minute : o.marks.hour;
+			//	$.anaclog.radial( ctx, r, i/60, mark );
+			//}
+			// draw hands
+			//for ( var name in o.hands ) {
+			//	var hand = o.hands[name];
+			//	$.anaclog.radial( ctx, r, hand.value(now), hand );
+			//}
+		});
+	};
+	$.fn.staticArc = function( o ){
+		o = $.extend({
+			radius:50,
+			segments : {
+				a: { color:'#b0c878', radius:0.5, start:35, end:40, unit:'Minutes' },
+				b: { color:'#d8df93', radius:0.5, start:55, end:45, unit:'Seconds' }
+			}
+		}, o );
+		return this.each(function(){ // initialise each arc
+			
+			var self = this, 
+				$self = $(this), 
+				ctx = self.getContext("2d"), 
+				r = o.radius;
+				
+			self.setAttribute("width",r*2);
+			self.setAttribute("height",r*2);
+			self.style.width = r*2 + "px";
+			self.style.height = r*2 + "px";
+			
+			$self.bind();
+			
+			
+			var now = new Date();
+			
+			//clear last frame
+			//ctx.clearRect( 0, 0, 2*r, 2*r );
+			
+			// draw timers
+			for ( var name in o.segments ) {
+				var segment = o.segments[name];
+				$.anaclog.checkTimer($self, now, segment);
+				$.anaclog.segment( ctx, r, segment );
+			}
+			// draw ticks
+			//for ( var i = 0; i < 60; i++ ) {
+			//	var mark = (i%5) ? o.marks.minute : o.marks.hour;
+			//	$.anaclog.radial( ctx, r, i/60, mark );
+			//}
+			// draw hands
+			//for ( var name in o.hands ) {
+			//	var hand = o.hands[name];
+			//	$.anaclog.radial( ctx, r, hand.value(now), hand );
+			//}
+		});
 	};
 	$.fn.anaclog = function( o ){
 		
@@ -90,12 +242,12 @@
 				// draw ticks
 				for ( var i = 0; i < 60; i++ ) {
 					var mark = (i%5) ? o.marks.minute : o.marks.hour;
-					$.anaclog.radial( ctx, r, i/60, mark );
+					$.anaclog.radialFraction( ctx, r, i/60, mark );
 				}
 				// draw hands
 				for ( var name in o.hands ) {
 					var hand = o.hands[name];
-					$.anaclog.radial( ctx, r, hand.value(now), hand );
+					$.anaclog.radialFraction( ctx, r, hand.value(now), hand );
 				}
 				
 			}, o.interval );
